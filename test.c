78822e223c9c146660446079a34599c870b46d57
oclyke <oclyke@gmail.com>
2022-03-07 10:35:39 -0800

    check f1 with input over 10

--- test.c
+++ test.c
@@ -4,6 +4,7 @@
 #include "lib.h"
 
 int main() {
+  assert(f1(20) == 21);
   assert(f1(5) == 2);
   assert(f2() == 1); // error in tutorial using post-increment after return - result should be 1 not 2
 }
