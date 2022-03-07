c58b28bf6cdd6eb9a0cfe89e600105c6d7bf83ac
oclyke <oclyke@gmail.com>
2022-03-07 12:55:04 -0800

    run more tests

--- test.c
+++ test.c
@@ -2,9 +2,13 @@
 
 #include<assert.h>
 #include "lib.h"
+#include "otherlib.h"
+#include "dilip.h"
 
 int main() {
   assert(f1(20) == 21);
   assert(f1(5) == 2);
   assert(f2() == 1); // error in tutorial using post-increment after return - result should be 1 not 2
+  assert(f3(43) == 1);
+  assert(f() == 1);
 }
